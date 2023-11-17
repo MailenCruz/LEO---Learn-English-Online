2bd78b204153e1a749432ba047db974e84c9eced
MailenCruz <138177328+MailenCruz@users.noreply.github.com>
2023-11-17 11:57:28 -0300

    viajero-ejercicios

--- src/app/components/viajero/alojamiento-viajero/opciones-alojamiento/multiple-choice-preguntas-alojamiento/multiple-choice-preguntas-alojamiento.component.ts
+++ src/app/components/viajero/alojamiento-viajero/opciones-alojamiento/multiple-choice-preguntas-alojamiento/multiple-choice-preguntas-alojamiento.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./multiple-choice-preguntas-alojamiento.component.css']
 })
 export class MultipleChoicePreguntasAlojamientoComponent {
+  preguntas = [
+    {
+      oracion: "Where is the extra ____?",
+      respuestas: ["Twin room","Pillow","Single room"],
+      respuestaCorrecta: "Pillow"
+    },
+    {
+      oracion: "Can I get a ____ to access my room?",
+      respuestas: ["Key card","Confirmation number","Reservation"],
+      respuestaCorrecta: "Key card"
+    }
+  ];
+
+  preguntaActual: number = 0;
+  respuestaSeleccionada: string = '';
+  respuestaCorrecta: string = '';
+  intentos: number = 0;
+  mostrarCuestionario: boolean = true;
+
+  verificarRespuesta() {
+    this.intentos++;
+    this.respuestaCorrecta = this.respuestaSeleccionada === this.preguntas[this.preguntaActual].respuestaCorrecta ? 'correcta' : 'incorrecta';
+  }
+
+  resetearEstado() {
+    this.respuestaSeleccionada = '';
+    this.respuestaCorrecta = '';
+  }
+
+  siguientePregunta() {
+    this.preguntaActual++;
+    if (this.preguntaActual >= this.preguntas.length) {
+      // Si no hay más preguntas, ocultar el cuestionario
+      this.mostrarCuestionario = false;
+      console.log("¡Fin del cuestionario!");
+    } else {
+      // Si hay más preguntas, reiniciar el estado
+      this.resetearEstado();
+    }
+  }
 
 }
--- src/app/components/viajero/restaurante-viajero/opciones-restaurante/multiple-choice-vocabulario-restaurante/multiple-choice-vocabulario-restaurante.component.ts
+++ src/app/components/viajero/restaurante-viajero/opciones-restaurante/multiple-choice-vocabulario-restaurante/multiple-choice-vocabulario-restaurante.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class MultipleChoiceVocabularioRestauranteComponent {
 
+  oraciones = [
+    {
+      oracion: "The ____ is delicious.",
+      respuestas: ["food","dish","dessert"],
+      "respuestaCorrecta": "food"
+    },
+    {
+      oracion: "I'd like the ______, please.",
+      respuestas: ["chicken","salad","pasta"],
+      respuestaCorrecta: "chicken"
+    }
+  ];
+
+  oracionActual: number = 0;
+  respuestaSeleccionada: string = '';
+  respuestaCorrecta: string = '';
+  intentos: number = 0;
+  mostrarCuestionario: boolean = true;
+
+  verificarRespuesta() {
+    this.intentos++;
+    this.respuestaCorrecta = this.respuestaSeleccionada === this.oraciones[this.oracionActual].respuestaCorrecta ? 'correcta' : 'incorrecta';
+  }
+
+  resetearEstado() {
+    this.respuestaSeleccionada = '';
+    this.respuestaCorrecta = '';
+  }
+
+  siguientePregunta() {
+    this.oracionActual++;
+    if (this.oracionActual >= this.oraciones.length) {
+      // Si no hay más preguntas, ocultar el cuestionario
+      this.mostrarCuestionario = false;
+      console.log("¡Fin del cuestionario!");
+    } else {
+      // Si hay más preguntas, reiniciar el estado
+      this.resetearEstado();
+    }
+  }
 }
