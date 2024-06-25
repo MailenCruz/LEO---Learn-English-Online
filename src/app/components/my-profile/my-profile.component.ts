7f49fb52241e70f1ec48f047f3f53dd0bb3aeaa7
MailenCruz <c.mailen.188@gmail.com>
2024-06-25 18:47:16 -0300

    modificar contraseña

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { UsersService } from 'src/app/services/users.service';
@@ -15,7 +15,15 @@ export class MyProfileComponent implements OnInit {
   form!: FormGroup;
   errorMail!: string;
   errorUser!: string;
+  errorCurrentPassword!: string;
+  errorNewPassword!: string;
   user!: User | undefined;
+
+  showCurrentPassword: boolean = false;
+  showNewPassword: boolean = false;
+
+  currentPassword: string = '';
+
 
   constructor(private userService: UsersService, private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute) { }
 
@@ -27,9 +35,47 @@ export class MyProfileComponent implements OnInit {
 
   initForm() {
     this.form = this.formBuilder.group({
-      editUsername: ['', [Validators.required, Validators.minLength(3)]],
-      editEmail: ['', [Validators.required, Validators.email]]
+      editUsername: ['', [Validators.required, Validators.minLength(3), this.validarFormatoUsername()]],
+      editEmail: ['', [Validators.required, this.validarFormatoEmail()]],
+      currentPassword: [''],
+      newPassword: ['', [Validators.minLength(6), this.validarFormatoPassword()]]
     })
+  }
+
+  ///FUNCIONES PARA CREAR NUESTROS VALIDATORS
+  validarFormatoPassword(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const value = control.value;
+      if (!value) {
+        return null; // no se valida si el campo está vacío
+      }
+      const containsNumber = /\d/.test(value);
+      return containsNumber ? null : { 'noNumber': true };
+    };
+  }
+
+  validarFormatoEmail(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const email = control.value;
+      if (!email) {
+        return null;
+      }
+      const formato = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+      const valido = formato.test(email);
+      return valido ? null : { 'invalidEmailDomain': true };
+    };
+  }
+
+  validarFormatoUsername(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const username = control.value;
+      if (!username) {
+        return null;
+      }
+      const formato = /^[A-Za-z]{2,}[A-Za-z0-9]*$/;
+      const valido = formato.test(username);
+      return valido ? null : { 'invalidUsername': true };
+    };
   }
 
   camposFormulario() {
@@ -41,10 +87,11 @@ export class MyProfileComponent implements OnInit {
           this.user = user;
 
           if (this.user) {
-            this.form = this.formBuilder.group({
-              editUsername: [this.user.username],
-              editEmail: [this.user.email]
+            this.form.patchValue({
+              editUsername: this.user.username,
+              editEmail: this.user.email
             });
+            this.currentPassword = this.user.password;
           } else {
             console.error('Usuario no encontrado');
           }
@@ -70,6 +117,7 @@ export class MyProfileComponent implements OnInit {
     );
   }
 
+  /*
   editarUser() {
     if (!this.user) return;
   
@@ -97,8 +145,65 @@ export class MyProfileComponent implements OnInit {
     }
 
   }
-  
+  */
+  editarUser() {
+    if (!this.user) return;
+
+    const newUsername = this.form.get('editUsername')!.value;
+    const newEmail = this.form.get('editEmail')!.value;
+    const auxCurrentPassword = this.form.get('currentPassword')!.value;
+    const auxNewPassword = this.form.get('newPassword')!.value;
+
+    const observables = [];
+
+
+    if (newUsername !== this.user.username) {
+      this.userService.verificarUser(newUsername).subscribe(result => {
+        this.errorUser = result;
+        if (this.errorUser === 'exito') {
+          if (newUsername) {
+            this.user!.username = newUsername;
+          }
+          this.actualizarUsuario();
+        }
+      })
+    }
+
+    if (newEmail !== this.user.email) {
+      this.userService.verificarEmail(newEmail).subscribe(result => {
+        this.errorMail = result;
+        if (this.errorMail === 'exito') {
+          if (newEmail) {
+            this.user!.email = newEmail;
+          }
+          this.actualizarUsuario();
+        }
+      })
+    }
+
+    if (auxCurrentPassword && auxNewPassword) {
+      if (auxCurrentPassword !== this.currentPassword) {
+        this.errorCurrentPassword = 'La contraseña actual no coincide.';
+      } else if (auxNewPassword === auxCurrentPassword) {
+        this.errorCurrentPassword = 'La nueva contraseña es igual a la actual.';
+      } else {
+        this.user.password = auxNewPassword;
+        this.actualizarUsuario();
+      }
+    }
+  }
+
+  toggleCurrentPasswordVisibility() {
+    this.showCurrentPassword = !this.showCurrentPassword;
+  }
+
+  toggleNewPasswordVisibility() {
+    this.showNewPassword = !this.showNewPassword;
+  }
+
   private actualizarUsuario() {
+
+
     if (this.user) {
       this.userService.putUser(this.user).subscribe({
         next: () => {
@@ -110,8 +215,8 @@ export class MyProfileComponent implements OnInit {
       });
     }
   }
-  
-  
+
+
 
 
 
